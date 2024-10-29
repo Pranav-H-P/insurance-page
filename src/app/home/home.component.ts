c28b69514d37ad5aa48d64b5a73b8e84c5240244
Pranav <pranavhp871@gmail.com>
2024-10-29 16:05:26 +0530

    Created not found page and hero page with responsiveness

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "../sections/hero-section/hero-section.component";
+import { FeaturesComponent } from "../sections/features/features.component";
+import { CallToActionComponent } from "../sections/call-to-action/call-to-action.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroSectionComponent, FeaturesComponent, CallToActionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
