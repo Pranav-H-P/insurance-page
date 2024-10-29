eafcd0c125630c4dc74faad21fba1b2f206d1b7b
Pranav <pranavhp871@gmail.com>
2024-10-29 23:16:41 +0530

    Completed call to action component

--- src/app/sections/call-to-action/call-to-action.component.ts
+++ src/app/sections/call-to-action/call-to-action.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-call-to-action',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './call-to-action.component.html',
   styleUrl: './call-to-action.component.scss'
 })
 export class CallToActionComponent {
 
+  userEmail: string = "";
+
+  sendToAPI(){
+    const emailRegex: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+    if (emailRegex.test(this.userEmail)){
+      alert(`Your Email: ${this.userEmail} has been successfully added!`);
+    }else{
+      alert("Please enter a valid email!");
+    }
+    
+  }
 }
